00d6d8aaba7a200d89eec353fac6242c37cfc214
PabloXOC <paxoc01@gmail.com>
2024-09-22 15:03:02 +0200

    added reformat null

--- srcs/more_reformats.c
+++ srcs/more_reformats.c
@@ -6,7 +6,7 @@
 /*   By: paxoc01 <paxoc01@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 11:44:10 by paxoc01           #+#    #+#             */
-/*   Updated: 2024/09/21 14:43:22 by paxoc01          ###   ########.fr       */
+/*   Updated: 2024/09/22 15:02:26 by paxoc01          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,15 +109,19 @@ int	reformat_final(t_data *data, t_spec *spec)
 	com = spec->command_list;
 	while (com != NULL)
 	{
-		if (expand_squiggle(data, &com->content, 0) == MALLOC_ERROR)
-			return (MALLOC_ERROR);
-		if (ft_remove_quotes_bar(&com->content, data) == MALLOC_ERROR)
-			return (MALLOC_ERROR);
-		if (ft_expand_first_word(data, com) == MALLOC_ERROR)
-			return (MALLOC_ERROR);
-		com->full_path = find_command_path(data->env, com->content[0], 0, data);
-		if (com->full_path == NULL)
-			return (error_i(MALLOC_ERROR, data));
+		if (com->content != NULL)
+		{
+			if (expand_squiggle(data, &com->content, 0) == MALLOC_ERROR)
+				return (MALLOC_ERROR);
+			if (ft_remove_quotes_bar(&com->content, data) == MALLOC_ERROR)
+				return (MALLOC_ERROR);
+			if (ft_expand_first_word(data, com) == MALLOC_ERROR)
+				return (MALLOC_ERROR);
+			com->full_path = find_command_path(data->env,
+					com->content[0], 0, data);
+			if (com->full_path == NULL)
+				return (error_i(MALLOC_ERROR, data));
+		}
 		com = com->next;
 	}
 }
